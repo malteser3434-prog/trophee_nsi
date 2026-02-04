a67015fdf5e54f952c120067c6a3edefc29d2051
TaLeon-gg <sachal34660@gmail.com>
2026-02-04 10:51:24 +0100

    Code du site Flask

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template  # On ajoute render_template ici
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    # render_template va chercher automatiquement dans le dossier /templates
-    return render_template('index.html')
+@app.route("/")
+def accueil():
+    return render_template("index.html")
 
-if __name__ == '__main__':
+@app.route("/enigme1", methods=["GET", "POST"])
+def enigme1():
+    message = ""
+    if request.method == "POST":
+        if request.form.get("reponse") == "42":
+            message = "Bravo ! Prochaine énigme : /enigme2xg"
+        else:
+            message = "Mauvaise réponse"
+
+    return render_template("enigme1.html", message=message)
+
+if __name__ == "__main__":
     app.run(debug=True)
